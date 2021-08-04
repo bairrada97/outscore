646ba4765b61f6747fa48191a22eff9a611073f0
joao <bairrada1997@gmail.com>
2021-08-04 13:30:23 +0200

    add BetsHelper module

--- components/MatchBetsHelper/MatchBetsHelper.vue
+++ components/MatchBetsHelper/MatchBetsHelper.vue
@@ -13,10 +13,9 @@
 
 <script>
 	import { reactive, watch, computed, ref, useFetch } from "@nuxtjs/composition-api";
-	import useCalendar from "../../modules/useCalendar";
 	import store from "@/store.js";
 	import CardStats from "@/components/CardStats/CardStats.vue";
-	import useH2H from "@/modules/useH2H";
+	import useBetsHelper from "@/modules/useBetsHelper";
 
 	export default {
 		components: {
@@ -28,125 +27,20 @@
 			}
 		},
 		setup(props) {
-			const { loadH2H, awayTeamH2H, homeTeamH2H, h2h } = useH2H();
-			const getHomeTeamH2H = computed(() => store.getHomeTeamH2H());
-			const getAwayTeamH2H = computed(() => store.getAwayTeamH2H());
-			const lastGamesLength = 5;
-			const betsHelper = computed(() => {
-				const lasthomeGames = [...getHomeTeamH2H.value].filter((item, index) => item.fixture.status.short == "FT").filter((item, index) => index < lastGamesLength);
-				const lastawayGames = [...getAwayTeamH2H.value].filter((item, index) => item.fixture.status.short == "FT").filter((item, index) => index < lastGamesLength);
-
-				const uniqueMatches = [...new Map([...lasthomeGames, ...lastawayGames].map(item => [item.fixture.id, item])).values()];
-
-				return uniqueMatches.reduce((acc, stats) => {
-					getGoals(acc, stats, 1.5);
-					getGoals(acc, stats, 2.5);
-					getGoals(acc, stats, 3.5);
-					getGoals(acc, stats, 4.5);
-					getGoals(acc, stats, 5.5);
-					getBothTeamsToScore(acc, stats);
-					getCleanSheet(acc, stats);
-
-					return acc;
-				}, {});
-			});
-
-			const getGoals = (acc, stats, value) => {
-				let homeTeam = computed(() => props.matchDetail.teams.home);
-				let awayTeam = computed(() => props.matchDetail.teams.away);
-				const homeTeamSide = Object.values(stats.teams).find(team => team.id === homeTeam.value.id);
-				const awayTeamSide = Object.values(stats.teams).find(team => team.id === awayTeam.value.id);
-				const goalsInGame = stats.goals.home + stats.goals.away;
-
-				acc["goals"] = acc["goals"] || {};
-				acc["goals"][`+${value}`] = acc["goals"][`+${value}`] || {};
-				acc["goals"][`+${value}`].home = acc["goals"][`+${value}`].home || {};
-				acc["goals"][`+${value}`].away = acc["goals"][`+${value}`].away || {};
-				acc["goals"][`+${value}`].home.name = props.matchDetail.teams.home.name;
-				acc["goals"][`+${value}`].away.name = props.matchDetail.teams.away.name;
-				acc["goals"][`+${value}`].home.value = acc["goals"][`+${value}`].home.value || 0;
-				acc["goals"][`+${value}`].away.value = acc["goals"][`+${value}`].away.value || 0;
-
-				if (acc["goals"][`+${value}`].home.name == homeTeamSide?.name) {
-					if (goalsInGame > value) {
-						acc["goals"][`+${value}`].home.value++;
-					}
-				}
-				if (acc["goals"][`+${value}`].away.name == awayTeamSide?.name) {
-					if (goalsInGame > value) {
-						acc["goals"][`+${value}`].away.value++;
-					}
-				}
-			};
-
-			const getBothTeamsToScore = (acc, stats) => {
-				let homeTeam = computed(() => props.matchDetail.teams.home);
-				let awayTeam = computed(() => props.matchDetail.teams.away);
-				const homeTeamSide = Object.values(stats.teams).find(team => team.id === homeTeam.value.id);
-				const awayTeamSide = Object.values(stats.teams).find(team => team.id === awayTeam.value.id);
-				const goalsHome = stats.goals.home;
-				const goalsAway = stats.goals.away;
-
-				acc["Both Teams To Score"] = acc["Both Teams To Score"] || {};
-				acc["Both Teams To Score"][`yes`] = acc["Both Teams To Score"][`yes`] || {};
-				acc["Both Teams To Score"][`yes`].home = acc["Both Teams To Score"][`yes`].home || {};
-				acc["Both Teams To Score"][`yes`].away = acc["Both Teams To Score"][`yes`].away || {};
-				acc["Both Teams To Score"][`yes`].home.name = props.matchDetail.teams.home.name;
-				acc["Both Teams To Score"][`yes`].away.name = props.matchDetail.teams.away.name;
-				acc["Both Teams To Score"][`yes`].home.value = acc["Both Teams To Score"][`yes`].home.value || 0;
-				acc["Both Teams To Score"][`yes`].away.value = acc["Both Teams To Score"][`yes`].away.value || 0;
-
-				if (acc["Both Teams To Score"][`yes`].home.name == homeTeamSide?.name) {
-					if (goalsHome > 0 && goalsAway > 0) {
-						acc["Both Teams To Score"][`yes`].home.value++;
-					}
-				}
-				if (acc["Both Teams To Score"][`yes`].away.name == awayTeamSide?.name) {
-					if (goalsHome > 0 && goalsAway > 0) {
-						acc["Both Teams To Score"][`yes`].away.value++;
-					}
-				}
-			};
-
-			const getCleanSheet = (acc, stats) => {
-				let homeTeam = computed(() => props.matchDetail.teams.home);
-				let awayTeam = computed(() => props.matchDetail.teams.away);
-				const homeTeamSide = Object.values(stats.teams).find(team => team.id === homeTeam.value.id);
-				const awayTeamSide = Object.values(stats.teams).find(team => team.id === awayTeam.value.id);
-				const goalsHome = stats.goals.home;
-				const goalsAway = stats.goals.away;
-
-				acc["Clean Sheet"] = acc["Clean Sheet"] || {};
-				acc["Clean Sheet"][`yes`] = acc["Clean Sheet"][`yes`] || {};
-				acc["Clean Sheet"][`yes`].home = acc["Clean Sheet"][`yes`].home || {};
-				acc["Clean Sheet"][`yes`].away = acc["Clean Sheet"][`yes`].away || {};
-				acc["Clean Sheet"][`yes`].home.name = props.matchDetail.teams.home.name;
-				acc["Clean Sheet"][`yes`].away.name = props.matchDetail.teams.away.name;
-				acc["Clean Sheet"][`yes`].home.value = acc["Clean Sheet"][`yes`].home.value || 0;
-				acc["Clean Sheet"][`yes`].away.value = acc["Clean Sheet"][`yes`].away.value || 0;
-
-				if (acc["Clean Sheet"][`yes`].home.name == homeTeamSide?.name) {
-					if (goalsAway == 0) {
-						acc["Clean Sheet"][`yes`].home.value++;
-					}
-				}
-				if (acc["Clean Sheet"][`yes`].away.name == awayTeamSide?.name) {
-					if (goalsHome == 0) {
-						acc["Clean Sheet"][`yes`].away.value++;
-					}
-				}
-			};
+			const { loadBetsHelper, betsHelper } = useBetsHelper();
+			const { home, away } = props.matchDetail.teams;
+			const lastGamesLength = 3;
 
 			const { fetch, fetchState } = useFetch(async () => {
-				await loadH2H(props.matchDetail.teams);
+				await loadBetsHelper(home, away);
 			});
 
 			fetch();
 
 			return {
-				betsHelper,
 				lastGamesLength,
-				fetchState
+				fetchState,
+				betsHelper
 			};
 		}
 	};
---
+++ modules/useBetsHelper.js
@@ -0,0 +1,226 @@
+import { defineComponent, reactive, computed, ref, onMounted, useFetch, onActivated, onUnmounted } from "@nuxtjs/composition-api";
+import store from "@/store.js";
+import axios from "axios";
+
+export default function () {
+	const betsHelperResponse = ref([]);
+	const homeTeam = ref(null);
+	const awayTeam = ref(null);
+	const loadBetsHelper = async (home, away) => {
+		try {
+			await axios.get(`https://api-football-v3.herokuapp.com/api/v3/betshelper/?home=${home.id}&away=${away.id}`).then(response => {
+				// betsHelper.value = store.getBetsHelper();
+				// const hasDataUpdated = !betsHelper.value.cacheDate || response.data.cacheDate != betsHelper.value.cacheDate;
+				// if (hasDataUpdated) store.setLiveGames(response.data);
+				betsHelperResponse.value = response.data;
+				homeTeam.value = home;
+				awayTeam.value = away;
+			});
+		} catch (error) {}
+	};
+
+	const betsHelper = computed(() => {
+		if (betsHelperResponse.value.length == 0 || betsHelperResponse.value == undefined) return;
+		const { away, home, h2h } = betsHelperResponse.value;
+		const uniqueMatches = [...new Map([...away, ...home, ...h2h].map(item => [item.fixture.id, item])).values()];
+
+		return uniqueMatches?.reduce((acc, stats) => {
+			getGoals(acc, stats, 1.5);
+			getGoals(acc, stats, 2.5);
+			getGoals(acc, stats, 3.5);
+			getGoals(acc, stats, 4.5);
+			getGoals(acc, stats, 5.5);
+			getBothTeamsToScore(acc, stats);
+			getCleanSheet(acc, stats);
+			getFirstTeamToScore(acc, stats);
+			getTotalCorners(acc, stats, 7.5);
+			getTotalCorners(acc, stats, 8.5);
+			getTotalCorners(acc, stats, 9.5);
+			getTotalCorners(acc, stats, 10.5);
+			getTotalCorners(acc, stats, 11.5);
+			getTotalCorners(acc, stats, 12.5);
+			getTotalCards(acc, stats, 2.5);
+			getTotalCards(acc, stats, 3.5);
+			getTotalCards(acc, stats, 4.5);
+			getTotalCards(acc, stats, 5.5);
+			getTotalCards(acc, stats, 6.5);
+			getTotalCards(acc, stats, 7.5);
+			return acc;
+		}, {});
+	});
+
+	const getGoals = (acc, stats, value) => {
+		const homeTeamSide = Object.values(stats.teams).find(team => team.id === homeTeam.value.id);
+		const awayTeamSide = Object.values(stats.teams).find(team => team.id === awayTeam.value.id);
+		const goalsInGame = stats.goals.home + stats.goals.away;
+
+		acc["goals"] = acc["goals"] || {};
+		acc["goals"][`+${value}`] = acc["goals"][`+${value}`] || {};
+		acc["goals"][`+${value}`].home = acc["goals"][`+${value}`].home || {};
+		acc["goals"][`+${value}`].away = acc["goals"][`+${value}`].away || {};
+		acc["goals"][`+${value}`].home.name = homeTeam.value.name;
+		acc["goals"][`+${value}`].away.name = awayTeam.value.name;
+		acc["goals"][`+${value}`].home.value = acc["goals"][`+${value}`].home.value || 0;
+		acc["goals"][`+${value}`].away.value = acc["goals"][`+${value}`].away.value || 0;
+
+		if (acc["goals"][`+${value}`].home.name == homeTeamSide?.name) {
+			if (goalsInGame > value) {
+				acc["goals"][`+${value}`].home.value++;
+			}
+		}
+		if (acc["goals"][`+${value}`].away.name == awayTeamSide?.name) {
+			if (goalsInGame > value) {
+				acc["goals"][`+${value}`].away.value++;
+			}
+		}
+	};
+
+	const getBothTeamsToScore = (acc, stats) => {
+		const homeTeamSide = Object.values(stats.teams).find(team => team.id === homeTeam.value.id);
+		const awayTeamSide = Object.values(stats.teams).find(team => team.id === awayTeam.value.id);
+		const goalsHome = stats.goals.home;
+		const goalsAway = stats.goals.away;
+
+		acc["Both Teams To Score"] = acc["Both Teams To Score"] || {};
+		acc["Both Teams To Score"][`yes`] = acc["Both Teams To Score"][`yes`] || {};
+		acc["Both Teams To Score"][`yes`].home = acc["Both Teams To Score"][`yes`].home || {};
+		acc["Both Teams To Score"][`yes`].away = acc["Both Teams To Score"][`yes`].away || {};
+		acc["Both Teams To Score"][`yes`].home.name = homeTeam.value.name;
+		acc["Both Teams To Score"][`yes`].away.name = awayTeam.value.name;
+		acc["Both Teams To Score"][`yes`].home.value = acc["Both Teams To Score"][`yes`].home.value || 0;
+		acc["Both Teams To Score"][`yes`].away.value = acc["Both Teams To Score"][`yes`].away.value || 0;
+
+		if (acc["Both Teams To Score"][`yes`].home.name == homeTeamSide?.name) {
+			if (goalsHome > 0 && goalsAway > 0) {
+				acc["Both Teams To Score"][`yes`].home.value++;
+			}
+		}
+		if (acc["Both Teams To Score"][`yes`].away.name == awayTeamSide?.name) {
+			if (goalsHome > 0 && goalsAway > 0) {
+				acc["Both Teams To Score"][`yes`].away.value++;
+			}
+		}
+	};
+
+	const getCleanSheet = (acc, stats) => {
+		const homeTeamSide = Object.values(stats.teams).find(team => team.id === homeTeam.value.id);
+		const awayTeamSide = Object.values(stats.teams).find(team => team.id === awayTeam.value.id);
+		const goalsHome = stats.goals.home;
+		const goalsAway = stats.goals.away;
+
+		acc["Clean Sheet"] = acc["Clean Sheet"] || {};
+		acc["Clean Sheet"][`yes`] = acc["Clean Sheet"][`yes`] || {};
+		acc["Clean Sheet"][`yes`].home = acc["Clean Sheet"][`yes`].home || {};
+		acc["Clean Sheet"][`yes`].away = acc["Clean Sheet"][`yes`].away || {};
+		acc["Clean Sheet"][`yes`].home.name = homeTeam.value.name;
+		acc["Clean Sheet"][`yes`].away.name = awayTeam.value.name;
+		acc["Clean Sheet"][`yes`].home.value = acc["Clean Sheet"][`yes`].home.value || 0;
+		acc["Clean Sheet"][`yes`].away.value = acc["Clean Sheet"][`yes`].away.value || 0;
+
+		if (acc["Clean Sheet"][`yes`].home.name == homeTeamSide?.name) {
+			if (goalsAway == 0) {
+				acc["Clean Sheet"][`yes`].home.value++;
+			}
+		}
+		if (acc["Clean Sheet"][`yes`].away.name == awayTeamSide?.name) {
+			if (goalsHome == 0) {
+				acc["Clean Sheet"][`yes`].away.value++;
+			}
+		}
+	};
+
+	const getFirstTeamToScore = (acc, stats) => {
+		const homeTeamSide = Object.values(stats.teams).find(team => team.id === homeTeam.value.id);
+		const awayTeamSide = Object.values(stats.teams).find(team => team.id === awayTeam.value.id);
+		const firstGoalEvent = stats.events.find(events => events.type == "Goal");
+
+		acc["First Team To Score"] = acc["First Team To Score"] || {};
+		acc["First Team To Score"][`yes`] = acc["First Team To Score"][`yes`] || {};
+		acc["First Team To Score"][`yes`].home = acc["First Team To Score"][`yes`].home || {};
+		acc["First Team To Score"][`yes`].away = acc["First Team To Score"][`yes`].away || {};
+		acc["First Team To Score"][`yes`].home.name = homeTeam.value.name;
+		acc["First Team To Score"][`yes`].away.name = awayTeam.value.name;
+		acc["First Team To Score"][`yes`].home.value = acc["First Team To Score"][`yes`].home.value || 0;
+		acc["First Team To Score"][`yes`].away.value = acc["First Team To Score"][`yes`].away.value || 0;
+
+		if (acc["First Team To Score"][`yes`].home.name == homeTeamSide?.name) {
+			if (firstGoalEvent?.team.id == homeTeam.value.id) {
+				acc["First Team To Score"][`yes`].home.value++;
+			}
+		}
+		if (acc["First Team To Score"][`yes`].away.name == awayTeamSide?.name) {
+			if (firstGoalEvent?.team.id == awayTeam.value.id) {
+				acc["First Team To Score"][`yes`].away.value++;
+			}
+		}
+	};
+
+	const getTotalCorners = (acc, stats, value) => {
+		const homeTeamSide = Object.values(stats.teams).find(team => team.id === homeTeam.value.id);
+		const awayTeamSide = Object.values(stats.teams).find(team => team.id === awayTeam.value.id);
+		const totalCorners =
+			stats.statistics.length > 0
+				? stats.statistics
+						.map(item => item.statistics)
+						.map(item2 => item2.filter(item3 => item3.type == "Corner Kicks"))
+						.reduce((prev, next) => prev[0].value + next[0].value)
+				: "";
+
+		acc["Total Corners"] = acc["Total Corners"] || {};
+		acc["Total Corners"][`+${value}`] = acc["Total Corners"][`+${value}`] || {};
+		acc["Total Corners"][`+${value}`].home = acc["Total Corners"][`+${value}`].home || {};
+		acc["Total Corners"][`+${value}`].away = acc["Total Corners"][`+${value}`].away || {};
+		acc["Total Corners"][`+${value}`].home.name = homeTeam.value.name;
+		acc["Total Corners"][`+${value}`].away.name = awayTeam.value.name;
+		acc["Total Corners"][`+${value}`].home.value = acc["Total Corners"][`+${value}`].home.value || 0;
+		acc["Total Corners"][`+${value}`].away.value = acc["Total Corners"][`+${value}`].away.value || 0;
+
+		if (acc["Total Corners"][`+${value}`].home.name == homeTeamSide?.name) {
+			if (totalCorners > value) {
+				acc["Total Corners"][`+${value}`].home.value++;
+			}
+		}
+		if (acc["Total Corners"][`+${value}`].away.name == awayTeamSide?.name) {
+			if (totalCorners > value) {
+				acc["Total Corners"][`+${value}`].away.value++;
+			}
+		}
+	};
+
+	const getTotalCards = (acc, stats, value) => {
+		const homeTeamSide = Object.values(stats.teams).find(team => team.id === homeTeam.value.id);
+		const awayTeamSide = Object.values(stats.teams).find(team => team.id === awayTeam.value.id);
+		const totalCorners =
+			stats.statistics.length > 0
+				? stats.statistics
+						.map(item => item.statistics)
+						.map(item2 => item2.filter(item3 => item3.type == "Yellow Cards" || item3.type == "Red Cards"))
+						.reduce((prev, next) => prev[0].value + next[0].value + prev[1].value + next[1].value)
+				: "";
+
+		acc["Total Cards"] = acc["Total Cards"] || {};
+		acc["Total Cards"][`+${value}`] = acc["Total Cards"][`+${value}`] || {};
+		acc["Total Cards"][`+${value}`].home = acc["Total Cards"][`+${value}`].home || {};
+		acc["Total Cards"][`+${value}`].away = acc["Total Cards"][`+${value}`].away || {};
+		acc["Total Cards"][`+${value}`].home.name = homeTeam.value.name;
+		acc["Total Cards"][`+${value}`].away.name = awayTeam.value.name;
+		acc["Total Cards"][`+${value}`].home.value = acc["Total Cards"][`+${value}`].home.value || 0;
+		acc["Total Cards"][`+${value}`].away.value = acc["Total Cards"][`+${value}`].away.value || 0;
+
+		if (acc["Total Cards"][`+${value}`].home.name == homeTeamSide?.name) {
+			if (totalCorners > value) {
+				acc["Total Cards"][`+${value}`].home.value++;
+			}
+		}
+		if (acc["Total Cards"][`+${value}`].away.name == awayTeamSide?.name) {
+			if (totalCorners > value) {
+				acc["Total Cards"][`+${value}`].away.value++;
+			}
+		}
+	};
+
+	return {
+		loadBetsHelper,
+		betsHelper
+	};
+}
